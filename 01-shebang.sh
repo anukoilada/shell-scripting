005cc8d91cdc965683c4c40accf81009f86fe885
anupama <anupama@gmail.com>
2023-03-25 01:13:37 -0400

    AutoPush

---
+++ 01-shebang.sh
@@ -0,0 +1,11 @@
+# 1 bin/bash
+
+# start of the line in bash scripting should always use a shebang notation
+
+# Apart from the first line none of the lines considered as shebang
+
+# Shebang notation should always start with #1
+
+# SHEBANG : Using this notation we are going to tell system that the interpreter to  be used for this script is bash or python or shell
+
+echo hello world
