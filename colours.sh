ae531afb1ae258baa8468b277fc0b8ccf5b7d6fe
anupama <anupama@gmail.com>
2023-03-25 01:51:56 -0400

    AutoPush

--- colours.sh
+++ colours.sh
@@ -1,11 +1,13 @@
 # /bin/bash
 
-Color	Foreground Code	Background Code
-Black           30      	40
-Red             31      	41
-Green           32      	42
-Yellow          33      	43
-Blue            34      	44
-Magenta         35      	45
-Cyan            36      	46
-White           37      	47
+#Color	Foreground Code	Background Code
+#Black           30      	40
+#Red             31      	41
+#Green           32      	42
+#Yellow          33      	43
+#Blue            34      	44
+#Magenta         35      	45
+#Cyan            36      	46
+#White           37      	47
+
+echo -e "\e[32m I am printing the green colour\e [0m"
