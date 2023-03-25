b5e110174d9cb7df2157e02e9612f06bd87b40fb
anupama <anupama@gmail.com>
2023-03-25 01:21:07 -0400

    AutoPush

--- 01-shebang.sh
+++ 01-shebang.sh
@@ -9,3 +9,4 @@
 # SHEBANG : Using this notation we are going to tell system that the interpreter to  be used for this script is bash or python or shell
 
 echo hello world
+echo This is the first session of the Bashgi
