8864b0f21c7e8ad5e3625920072070bf35820158
anupama <anupama@gmail.com>
2023-03-25 01:43:28 -0400

    AutoPush

---
+++ colours.sh
@@ -0,0 +1,11 @@
+# /bin/bash
+
+Color	Foreground Code	Background Code
+Black           30      	40
+Red             31      	41
+Green           32      	42
+Yellow          33      	43
+Blue            34      	44
+Magenta         35      	45
+Cyan            36      	46
+White           37      	47
