074eb9175aafe7938ce9ff4ea89dcb0b636aa4a8
anupama <anupama@gmail.com>
2023-03-25 21:57:46 -0400

    AutoPush

---
+++ 08specialvars.sh
@@ -0,0 +1,8 @@
+#1 /bin/bash
+
+echo $0    # Prints Script Name 
+echo $1    # Takes the first value from the command line 
+echo $2    # Takes the second value from the command line 
+
+echo $*    # $* is going to print the used variables  
+echo $@    # $@ is going to print the used variables
