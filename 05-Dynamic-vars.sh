b07e809d0765449649f9d8e1dca0627f857361df
anupama <anupama@gmail.com>
2023-03-25 21:25:31 -0400

    AutoPush

--- 05-Dynamic-vars.sh
+++ 05-Dynamic-vars.sh
@@ -1,9 +1,9 @@
 #1/bin/bash
 
-TODAYDATE=${date+%F}
+TODAYDATE=&{date+%F}
 
 echo -e "Welcome to Bash training. Today date is \e[32m ${TODAYDATE} \e[0m"
-echo "Number user sessions in the system are : ${who | wc -l}"
+echo "Number user sessions in the system are : &{who | wc -l}"
 
 
 
