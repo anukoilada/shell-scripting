a6f1ab369814680e8827ded38279ee9f5709cb7d
anupama <anupama@gmail.com>
2023-03-25 20:42:54 -0400

    AutoPush

--- 05-Dynamic-vars.sh
+++ 05-Dynamic-vars.sh
@@ -1,5 +1,5 @@
 #1 /bin/bash
 
-Today Date ="03-25-2023"
+TodayDate ="03-25-2023"
 
-echo "Welcome to Bash training. Today date is ${Today Date}"
+echo "Welcome to Bash training. Today date is ${TodayDate}"
