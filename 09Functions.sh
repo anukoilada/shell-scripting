1efe2e4fac920187ca95d1c2e45510c256b3aba6
anupama <anupama@gmail.com>
2023-03-25 23:06:36 -0400

    AutoPush

--- 09Functions.sh
+++ 09Functions.sh
@@ -5,3 +5,15 @@ sample() {
 }
 sample
 sample
+ stat() {
+    echo -e "\t Total number of sessons : $(who | wc -l)"
+    echo "Todays date is $(date +%F)"
+    echo "Load Average On The system is $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1}')"
+    echo -e "\t stat function completed"
+
+    echo "Calling sample function"
+    sample 
+ }
+
+ echo "calling stat function" 
+ stat
