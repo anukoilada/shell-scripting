0ccf07c3babc532bf092c4b7b0128996d3f40d69
anupama <anupama@gmail.com>
2023-03-27 23:02:28 -0400

    AutoPush

--- robot/components/redis.sh
+++ robot/components/redis.sh
@@ -1,3 +1,22 @@
 # /bin/bash 
 
-echo "I am redis"
+COMPONENT=redis
+
+echo -n "Configuring $COMPONENT repo :"
+curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>> $LOGFILE
+stat $? 
+
+echo -n "Installing $COMPONENT server :"
+yum install redis-6.2.11 -y  &>> $LOGFILE
+stat $? 
+
+echo -n "Updating the $COMPONENT visibility : "
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
+stat $? 
+
+echo -n "Performing Daemon-Reload : "
+systemctl daemon-reload  &>> $LOGFILE
+systemctl enable $COMPONENT   &>> $LOGFILE
+systemctl restart $COMPONENT   &>> $LOGFILE
+stat $?
