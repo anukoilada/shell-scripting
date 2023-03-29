27bfc8efb7e57d12b18c23edd43981ade89c8246
anupama <anupama@gmail.com>
2023-03-28 22:24:53 -0400

    AutoPush

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -1,89 +1,12 @@
 #1 /bin/bash
 
 COMPONENT=catalogue
-LOGFILE="/tmp/$COMPONENT.log"
-APPUSER=roboshop
 
-# Validting whether the executed user is a root user or not 
-ID=$(id -u)
+source components/common.sh    # Source is going to load the file, so that you can call all of them as per your need
 
-if [ "$ID" -ne 0 ] ; then 
-    echo -e "\e[31m You should execute this script as a root user or with a sudo as prefix \e[0m" 
-    exit 1
-fi 
+NODEJS                         # Calling NodeJS Function.
 
 
-stat() {
-    if [ $1 -eq 0 ] ; then 
-        echo -e "\e[32m Success \e[0m"
-    else 
-        echo -e "\e[31m Failure \e[0m"
-        exit 2
-    fi 
-}
-
-echo -n "Configuring the nodejs repo :"
-curl --silent --location https://rpm.nodesource.com/setup_16.x | bash - &>> $LOGFILE
-stat $?  
-
-echo -n "Installing NodeJS :"
-yum install nodejs -y &>> $LOGFILE
-stat $?
-
-    # Calling Create-User Functon
-
-    id $APPUSER  &>> $LOGFILE
-    if [ $? -ne 0 ] ; then 
-        echo -n "Creating the Application User Account :" 
-        useradd roboshop &>> $LOGFILE
-        stat $? 
-    fi 
-
-    
-
-    # Calling Download_And_Extract Function
-    
-
-    echo -n "Downloading the $COMPONENT component :"
-    curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
-    stat $? 
-
-    echo -n "Extracting the $COMPONENT in the $APPUSER directory"
-    cd /home/$APPUSER 
-    rm -rf /home/$APPUSER/$COMPONENT &>> $LOGFILE
-    unzip -o /tmp/$COMPONENT.zip  &>> $LOGFILE
-    stat $? 
-
-    echo -n "Configuring the permissions :"
-    mv /home/$APPUSER/$COMPONENT-main /home/$APPUSER/$COMPONENT
-    chown -R $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT
-    stat $?
-
-    
-
-    # Calling NPM Install Function
-    
-
-    echo -n "Installing the $COMPONENT Application :"
-    cd /home/$APPUSER/$COMPONENT/ 
-    npm install &>> $LOGFILE
-    stat $? 
-
-    
-
-    # Calling Config-Svc Function
-
-
-    echo -n "Updating the systemd file with DB Details :"
-    sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
-    mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
-    stat $? 
-
-    echo -n "Starting the $COMPONENT service : "
-    systemctl daemon-reload &>> $LOGFILE
-    systemctl enable $COMPONENT &>> $LOGFILE
-    systemctl restart $COMPONENT &>> $LOGFILE
-    stat $?
 
 
     
