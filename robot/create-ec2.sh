fc746fe40fb45a7e361356d3005894b6317ce5dd
anupama <anupama@gmail.com>
2023-04-04 21:06:31 -0400

    AutoPush

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -2,14 +2,15 @@
 
 # This is a script created to launch EC2 Servers and create the associated Route53 Record 
 
-if [ -z "$1" ]; then 
+if [ -z "$1" ] || [ -z "$2"]; then 
     echo -e "\e[31m Component Name is required \e[0m \t\t"
-    echo -e "\t\t\t \e[32m Sample Usage is : $ bash create-ec2.sh user \e[0m"
+    echo -e "\t\t\t \e[32m Sample Usage is : $ bash create-ec2.sh user dev \e[0m"
     exit 1
 fi 
 
-HOSTEDZONEID="Z09034203E2XOTY5BDYPO"
-COMPONENT=$1
+HOSTEDZONEID="Z096213084GYRYZXR9VK"
+COMPONENT=$1 
+ENV=$2
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b53-allow-all-sg  | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
@@ -17,20 +18,34 @@ SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b53-all
 echo -n "Ami ID is $AMI_ID"
 
 echo -n "Launching the instance with $AMI_ID as AMI :"
+
+create_server() {
     
     echo "*** Launching $COMPONENT Server ***"
 
     IPADDRESS=$(aws ec2 run-instances --image-id $AMI_ID \
-                    --instance-type t2.micro \
+                    --instance-type t3.micro \
                     --security-group-ids ${SGID} \
                     --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-                    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+                    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT-$ENV}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-    sed -e "s/COMPONENT/${COMPONENT}/" -e  "s/IPADDRESS/${IPADDRESS}/" robot/record.json > /tmp/record.json
-    aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch=file:///tmp/record.json | jq
+    sed -e "s/COMPONENT/${COMPONENT}-${ENV}/" -e  "s/IPADDRESS/${IPADDRESS}/" robot/record.json > /tmp/record.json
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/record.json | jq
 
     echo "*** $COMPONENT Server Completed ***"
 
+}
 
+if [ "$1" == "all" ] ; then 
+    for component in frontend mongodb catalogue cart user mysql redis rabbitmq shipping payment ; do  
+        COMPONENT=$component 
+        create_server
+    done
 
+else 
+        create_server
+
+fi 
+
+ 
  
